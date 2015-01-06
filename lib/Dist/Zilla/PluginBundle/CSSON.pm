ff9c3a4635a17aca7820e600a574623ee1e94e40
Erik Carlsson <info@code301.com>
2015-01-06 22:30:50 +0100

    Init

---
+++ lib/Dist/Zilla/PluginBundle/CSSON.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Dist::Zilla::PluginBundle::CSSON;
+
+# VERSION
+
+
+
+1;
+
+# ABSTRACT: Short intro
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+  use Dist::Zilla::PluginBundle::CSSON;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::PluginBundle::CSSON is ...
+
+=head1 SEE ALSO
+
+=cut
