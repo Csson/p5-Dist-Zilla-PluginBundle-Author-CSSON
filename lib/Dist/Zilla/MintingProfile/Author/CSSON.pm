ddf8c3ae86714880166e0b080f3cc04cf8d72c3c
Erik Carlsson <info@code301.com>
2016-02-18 22:13:09 +0000

    MintingProfile deprecation

--- lib/Dist/Zilla/MintingProfile/Author/CSSON.pm
+++ lib/Dist/Zilla/MintingProfile/Author/CSSON.pm
@@ -1,8 +1,17 @@
 package Dist::Zilla::MintingProfile::Author::CSSON;
 
-# VERSION
+# AUTHORITY
+our $VERSION = '0.1103';
 
 use Moose;
 with 'Dist::Zilla::Role::MintingProfile::ShareDir';
 
 1;
+
+__END__
+
+=head1 STATUS
+
+Deprecated. See L<Dist::Zilla::MintingProfile::Iller::CSSON> instead.
+
+=cut
